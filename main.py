dc33b28423b5a1cc8581025d9cc1697451b590a9
wzq215 <1800012611@pku.edu.cn>
2022-10-13 14:18:49 +0800

    Initial commit ps_read_hdf_3d.py: download and read data from MHDweb PSI

---
+++ main.py
@@ -0,0 +1,26 @@
+import numpy as np
+import tensorflow as tf
+
+def get_y(x):
+    return 10 + x*x
+
+
+def sample_data(n=10000, scale=100):
+    data = []
+
+    x = scale*(np.random.random_sample((n,))-0.5)
+
+    for i in range(n):
+        yi = get_y(x[i])
+        data.append([x[i], yi])
+
+    return np.array(data)
+
+def generator(Z,hsize=[16, 16],reuse=False):
+    with tf.variable_scope("GAN/Generator",reuse=reuse):
+        h1 = tf.layers.dense(Z,hsize[0],activation=tf.nn.leaky_relu)
+        h2 = tf.layers.dense(h1,hsize[1],activation=tf.nn.leaky_relu)
+        out = tf.layers.dense(h2,2)
+
+    return out
+
