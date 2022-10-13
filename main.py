596cca8fd376d45d45f05d36d1d33110db436cdb
wzq215 <1800012611@pku.edu.cn>
2022-10-13 16:16:45 +0800

    GAN based on Tensorflow2.0. according to https://blog.csdn.net/qq_36758914/article/details/104613596

--- main.py
+++ main.py
@@ -1,26 +1,114 @@
-import numpy as np
 import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import matplotlib.pyplot as plt
 
-def get_y(x):
-    return 10 + x*x
+(train_images, train_labels), (_, _) = keras.datasets.mnist.load_data()
+train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype('float32')
+train_images = (train_images - 127.5) / 127.5
+
+BATCH_SIZE = 256
+BUFFER_SIZE = 60000
+
+datasets = tf.data.Dataset.from_tensor_slices(train_images)
+datasets = datasets.shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
 
 
-def sample_data(n=10000, scale=100):
-    data = []
+def generator_model():
+    model = keras.Sequential()
+    model.add(layers.Dense(256, input_shape=(100,), use_bias=False))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
 
-    x = scale*(np.random.random_sample((n,))-0.5)
+    model.add(layers.Dense(512, use_bias=False))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
 
-    for i in range(n):
-        yi = get_y(x[i])
-        data.append([x[i], yi])
+    model.add(layers.Dense(28 * 28 * 1, use_bias=False, activation='tanh'))
+    model.add(layers.BatchNormalization())
 
-    return np.array(data)
+    model.add(layers.Reshape((28, 28, 1)))
 
-def generator(Z,hsize=[16, 16],reuse=False):
-    with tf.variable_scope("GAN/Generator",reuse=reuse):
-        h1 = tf.layers.dense(Z,hsize[0],activation=tf.nn.leaky_relu)
-        h2 = tf.layers.dense(h1,hsize[1],activation=tf.nn.leaky_relu)
-        out = tf.layers.dense(h2,2)
+    return model
 
-    return out
 
+def discriminator_model():
+    model = keras.Sequential()
+    model.add(layers.Flatten())
+
+    model.add(layers.Dense(512, use_bias=False))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Dense(256, use_bias=False))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Dense(1))
+
+    return model
+
+
+cross_entropy = keras.losses.BinaryCrossentropy(from_logits=True)
+
+
+def discriminator_loss(real_out, fake_out):
+    real_loss = cross_entropy(tf.ones_like(real_out), real_out)
+    fake_loss = cross_entropy(tf.zeros_like(fake_out), fake_out)
+    return real_loss + fake_loss
+
+
+def generator_loss(fake_out):
+    return cross_entropy(tf.ones_like(fake_out), fake_out)
+
+
+generator_opt = keras.optimizers.Adam(1e-4)
+discriminator_opt = keras.optimizers.Adam(1e-4)
+
+generator = generator_model()
+discriminator = discriminator_model()
+
+noise_dim = 100
+
+
+def train_step(images):
+    noise = tf.random.normal([BATCH_SIZE, noise_dim])
+
+    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+        real_out = discriminator(images, training=True)
+        gen_image = generator(noise, training=True)
+        fake_out = discriminator(gen_image, training=True)
+
+        gen_loss = generator_loss(fake_out)
+        disc_loss = discriminator_loss(real_out, fake_out)
+    gradient_gen = gen_tape.gradient(gen_loss, generator.trainable_variables)
+    gradient_disc = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
+    generator_opt.apply_gradients(zip(gradient_gen, generator.trainable_variables))
+    discriminator_opt.apply_gradients(zip(gradient_disc, discriminator.trainable_variables))
+
+
+def generate_plot_image(gen_model, test_noise):
+    pre_images = gen_model(test_noise, training=False)
+    fig = plt.figure(figsize=(4, 4))
+    for i in range(pre_images.shape[0]):
+        plt.subplot(4, 4, i + 1)
+        plt.imshow((pre_images[i, :, :, 0] + 1) / 2, cmap='gray')
+        plt.axis('off')
+
+
+EPOCHS = 100
+num_exp_to_generate = 16
+seed = tf.random.normal([num_exp_to_generate, noise_dim])
+
+
+def train(dataset, epochs):
+    for epoch in range(epochs):
+        for image_batch in dataset:
+            train_step(image_batch)
+            print('.', end='')
+        generate_plot_image(generator, seed)
+        plt.savefig('Figures/test_handwritten/' + str(epoch) + '.png')
+        plt.close()
+
+
+train(datasets, EPOCHS)
