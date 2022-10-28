87c3088921082c7fbebcf88da04465e21e55d1c9
wzq215 <1800012611@pku.edu.cn>
2022-10-28 16:36:02 +0800

    Add runable pix2pix_example.py

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ Course project For space weather forcasting.
 
 CORHEL MHD data is used in this project as input. There are two types of PSI data:
 
-* Medium: cr1625 to cr2240. Total 15 CRs. Not provided in the future. |
+* Medium: cr1625 to cr2240. Total 615 CRs. Not provided in the future. |
 * High: cr2235 to cr2262. Total 28 CRs. Higher resolution & New codes. | Too Few!!!
 
 Note that each High-CR has at least three sets of data: hmi_masp_mas_std_0201; hmi_mast_mas_std_0101;
--- pix2pix_origin.py
+++ pix2pix_origin.py
@@ -7,73 +7,80 @@ import time
 import datetime
 
 from matplotlib import pyplot as plt
-import numpy as np
 
-PATH = 'Data/tmp_data_magmap/'
-print(os.listdir(PATH))
+# from IPython import display
 
-sample_image = np.squeeze(np.load(PATH + 'train/magmap_2235_0.npy')[0, :, :, :])
+dataset_name = "facades"  # @param ["cityscapes", "edges2handbags", "edges2shoes", "facades", "maps", "night2day"]
+
+_URL = f'http://efrosgans.eecs.berkeley.edu/pix2pix/datasets/{dataset_name}.tar.gz'
+
+path_to_zip = tf.keras.utils.get_file(
+    fname=f"{dataset_name}.tar.gz",
+    origin=_URL,
+    extract=True)
+
+path_to_zip = pathlib.Path(path_to_zip)
+
+PATH = path_to_zip.parent / dataset_name
+
+print(list(PATH.parent.iterdir()))
+
+sample_image = tf.io.read_file(str(PATH / 'train/1.jpg'))
+sample_image = tf.io.decode_jpeg(sample_image)
+print(sample_image.shape)
 
 plt.figure()
-plt.imshow(sample_image / 100 + .5)
-plt.gca().invert_yaxis()
-plt.title('rtp(RNT) to RGB')
-plt.xlabel('lon [pixel]')
-plt.ylabel('lat [pixel]')
+plt.imshow(sample_image)
 plt.show()
 
 
-def load(file_name):
-    # print('----------load start-----------')
-    # print('file_name: ', file_name)
-    if type(file_name) != str:
-        file_name = file_name.numpy()
-        with open('loadedfiles.txt', 'a') as f:
-            f.write(str(file_name))
-    magmap_pair = np.load(file_name, allow_pickle=True)
-    # print('magmap_pair: ', magmap_pair)
-    input_image = np.squeeze(magmap_pair[0, :, :, :]) / 100 + 0.5
-    real_image = np.squeeze(magmap_pair[1, :, :, :]) / 100 + 0.5
+def load(image_file):
+    # Read and decode an image file to a uint8 tensor
+    print(image_file)
+    image = tf.io.read_file(image_file)
+    image = tf.io.decode_jpeg(image)
+
+    # Split each image tensor into two tensors:
+    # - one with a real building facade image
+    # - one with an architecture label image
+    w = tf.shape(image)[1]
+    w = w // 2
+    input_image = image[:, w:, :]
+    real_image = image[:, :w, :]
+
+    # Convert both images to float32 tensors
     input_image = tf.cast(input_image, tf.float32)
     real_image = tf.cast(real_image, tf.float32)
-    # print('input_image: ', input_image)
-    # print('real_image: ', real_image)
-    # print('----------load end-----------')
+    print('input_image: ', input_image)
+    print('real_image: ', real_image)
+
     return input_image, real_image
 
 
-print('--------visualize sample data---------')
-train_dataset = tf.data.Dataset.list_files(PATH + 'train/magmap_2235_0.npy')
-tf.data.Dataset.list_files(PATH + 'train/magmap_2235_0.npy')
-inp, re = load(PATH + 'train/magmap_2235_0.npy')
+inp, re = load(str(PATH / 'train/100.jpg'))
 # Casting to int for matplotlib to display the images
 plt.figure()
-plt.imshow(inp)
+plt.imshow(inp / 255.0)
 plt.figure()
-plt.imshow(re)
+plt.imshow(re / 255.0)
 plt.show()
-print('--------visualize sample data end---------')
 
 # The facade training set consist of 400 images
-BUFFER_SIZE = 4
+BUFFER_SIZE = 400
 # The batch size of 1 produced better results for the U-Net in the original pix2pix experiment
 BATCH_SIZE = 1
 # Each image is 256x256 in size
-IMG_WIDTH = 512
+IMG_WIDTH = 256
 IMG_HEIGHT = 256
 
 
 def resize(input_image, real_image, height, width):
-    # print('---------resize start--------')
-    # print(input_image)
-    input_image.set_shape([256, 512, 3])
-    real_image.set_shape([256, 512, 3])
-    # print(input_image)
+    print('To Resize: ', input_image)
     input_image = tf.image.resize(input_image, [height, width],
                                   method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
     real_image = tf.image.resize(real_image, [height, width],
                                  method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
-    # print('--------resize end----------')
+
     return input_image, real_image
 
 
@@ -87,19 +94,16 @@ def random_crop(input_image, real_image):
 
 # Normalizing the images to [-1, 1]
 def normalize(input_image, real_image):
-    # print('----------normalize start----------')
-    norm_max = tf.reduce_max([abs(tf.reduce_max(input_image)), abs(tf.reduce_min(input_image))])
-    # print('maxmium value for normalization: ', norm_max)
-    input_image = (input_image / norm_max)
-    real_image = (real_image / norm_max)
-    # print('---------normalize end-------------')
+    input_image = (input_image / 127.5) - 1
+    real_image = (real_image / 127.5) - 1
+
     return input_image, real_image
 
 
 @tf.function()
 def random_jitter(input_image, real_image):
     # Resizing to 286x286
-    input_image, real_image = resize(input_image, real_image, 280, 560)
+    input_image, real_image = resize(input_image, real_image, 286, 286)
 
     # Random cropping back to 256x256
     input_image, real_image = random_crop(input_image, real_image)
@@ -116,28 +120,21 @@ plt.figure(figsize=(6, 6))
 for i in range(4):
     rj_inp, rj_re = random_jitter(inp, re)
     plt.subplot(2, 2, i + 1)
-    plt.imshow(rj_inp)
+    plt.imshow(rj_inp / 255.0)
     plt.axis('off')
-    plt.title('random jitter')
 plt.show()
 
 
 def load_image_train(image_file):
-    # print('--------load image train start--------')
-    # print('image_file', image_file)
-    [input_image, real_image, ] = tf.py_function(func=load, inp=[image_file], Tout=[tf.float32, tf.float32])
-    # print(X)
-    # input_image, real_image = X
-    # print('input_image: ', input_image)
-    # print('real_image: ', real_image)
+    input_image, real_image = load(image_file)
     input_image, real_image = random_jitter(input_image, real_image)
     input_image, real_image = normalize(input_image, real_image)
-    # print('---------load iamge train end---------')
+
     return input_image, real_image
 
 
 def load_image_test(image_file):
-    [input_image, real_image] = tf.py_function(load, inp=[image_file], Tout=[tf.float32, tf.float32])
+    input_image, real_image = load(image_file)
     input_image, real_image = resize(input_image, real_image,
                                      IMG_HEIGHT, IMG_WIDTH)
     input_image, real_image = normalize(input_image, real_image)
@@ -145,19 +142,17 @@ def load_image_test(image_file):
     return input_image, real_image
 
 
-PATH = 'Data/tmp_data_magmap/'
-# train_file_list = os.listdir('Data/tmp_data_magmap/train/*.npy')
-# train_file_list = [PATH + 'train/' + fn for fn in train_file_list]
-# train_dataset = tf.data.Dataset.from_tensor_slices(train_file_list)
-train_dataset = tf.data.Dataset.list_files(PATH + 'train/*.npy')
-train_dataset = train_dataset.map(load_image_train)
+train_dataset = tf.data.Dataset.list_files(str(PATH / 'train/*.jpg'))
+print(train_dataset)
+train_dataset = train_dataset.map(load_image_train,
+                                  num_parallel_calls=tf.data.AUTOTUNE)
 train_dataset = train_dataset.shuffle(BUFFER_SIZE)
 train_dataset = train_dataset.batch(BATCH_SIZE)
 
 try:
-    test_dataset = tf.data.Dataset.list_files(PATH + 'test/*.npy')
+    test_dataset = tf.data.Dataset.list_files(str(PATH / 'test/*.jpg'))
 except tf.errors.InvalidArgumentError:
-    test_dataset = tf.data.Dataset.list_files(PATH + 'val/*.npy')
+    test_dataset = tf.data.Dataset.list_files(str(PATH / 'val/*.jpg'))
 test_dataset = test_dataset.map(load_image_test)
 test_dataset = test_dataset.batch(BATCH_SIZE)
 
@@ -211,12 +206,12 @@ print(up_result.shape)
 
 
 def Generator():
-    inputs = tf.keras.layers.Input(shape=[256, 512, 3])
+    inputs = tf.keras.layers.Input(shape=[256, 256, 3])
 
     down_stack = [
-        downsample(64, 4, apply_batchnorm=False),  # (batch_size, 128, 128, 64) (180,90)
-        downsample(128, 4),  # (batch_size, 64, 64, 128) (90,45)
-        downsample(256, 4),  # (batch_size, 32, 32, 256) (45,23)
+        downsample(64, 4, apply_batchnorm=False),  # (batch_size, 128, 128, 64)
+        downsample(128, 4),  # (batch_size, 64, 64, 128)
+        downsample(256, 4),  # (batch_size, 32, 32, 256)
         downsample(512, 4),  # (batch_size, 16, 16, 512)
         downsample(512, 4),  # (batch_size, 8, 8, 512)
         downsample(512, 4),  # (batch_size, 4, 4, 512)
@@ -246,7 +241,6 @@ def Generator():
     skips = []
     for down in down_stack:
         x = down(x)
-        # print(x)
         skips.append(x)
 
     skips = reversed(skips[:-1])
@@ -254,7 +248,6 @@ def Generator():
     # Upsampling and establishing the skip connections
     for up, skip in zip(up_stack, skips):
         x = up(x)
-        # print(x)
         x = tf.keras.layers.Concatenate()([x, skip])
 
     x = last(x)
@@ -262,11 +255,11 @@ def Generator():
     return tf.keras.Model(inputs=inputs, outputs=x)
 
 
-print('--------test generator--------')
 generator = Generator()
-tf.keras.utils.plot_model(generator, show_shapes=True, dpi=128)
+tf.keras.utils.plot_model(generator, show_shapes=True, dpi=64)
 
 gen_output = generator(inp[tf.newaxis, ...], training=False)
+print(inp[tf.newaxis, ...])
 plt.imshow(gen_output[0, ...])
 plt.show()
 
@@ -288,8 +281,8 @@ def generator_loss(disc_generated_output, gen_output, target):
 def Discriminator():
     initializer = tf.random_normal_initializer(0., 0.02)
 
-    inp = tf.keras.layers.Input(shape=[256, 512, 3], name='input_image')
-    tar = tf.keras.layers.Input(shape=[256, 512, 3], name='target_image')
+    inp = tf.keras.layers.Input(shape=[256, 256, 3], name='input_image')
+    tar = tf.keras.layers.Input(shape=[256, 256, 3], name='target_image')
 
     x = tf.keras.layers.concatenate([inp, tar])  # (batch_size, 256, 256, channels*2)
 
@@ -314,9 +307,8 @@ def Discriminator():
     return tf.keras.Model(inputs=[inp, tar], outputs=last)
 
 
-print('--------test discriminator----------')
 discriminator = Discriminator()
-tf.keras.utils.plot_model(discriminator, show_shapes=True, dpi=128)
+tf.keras.utils.plot_model(discriminator, show_shapes=True, dpi=64)
 
 disc_out = discriminator([inp[tf.newaxis, ...], gen_output], training=False)
 plt.imshow(disc_out[0, ..., -1], vmin=-20, vmax=20, cmap='RdBu_r')
@@ -334,7 +326,6 @@ def discriminator_loss(disc_real_output, disc_generated_output):
     return total_disc_loss
 
 
-print('-----------set optimizer & checkpoint---------')
 generator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
 discriminator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
 
@@ -393,10 +384,10 @@ def train_step(input_image, target, step):
                                                 discriminator.trainable_variables))
 
     with summary_writer.as_default():
-        tf.summary.scalar('gen_total_loss', gen_total_loss, step=step)
-        tf.summary.scalar('gen_gan_loss', gen_gan_loss, step=step)
-        tf.summary.scalar('gen_l1_loss', gen_l1_loss, step=step)
-        tf.summary.scalar('disc_loss', disc_loss, step=step)
+        tf.summary.scalar('gen_total_loss', gen_total_loss, step=step // 1000)
+        tf.summary.scalar('gen_gan_loss', gen_gan_loss, step=step // 1000)
+        tf.summary.scalar('gen_l1_loss', gen_l1_loss, step=step // 1000)
+        tf.summary.scalar('disc_loss', disc_loss, step=step // 1000)
 
 
 def fit(train_ds, test_ds, steps):
@@ -404,16 +395,16 @@ def fit(train_ds, test_ds, steps):
     start = time.time()
 
     for step, (input_image, target) in train_ds.repeat().take(steps).enumerate():
-        if (step) % 100 == 0:
+        if (step) % 1000 == 0:
             # display.clear_output(wait=True)
 
             if step != 0:
-                print(f'Time taken for 100 steps: {time.time() - start:.2f} sec\n')
+                print(f'Time taken for 1000 steps: {time.time() - start:.2f} sec\n')
 
             start = time.time()
 
             generate_images(generator, example_input, example_target)
-            print(f"Step: {step // 100}k")
+            print(f"Step: {step // 1000}k")
 
         train_step(input_image, target, step)
 
@@ -422,13 +413,12 @@ def fit(train_ds, test_ds, steps):
             print('.', end='', flush=True)
 
         # Save (checkpoint) the model every 5k steps
-        if (step + 1) % 100 == 0:
+        if (step + 1) % 5000 == 0:
             checkpoint.save(file_prefix=checkpoint_prefix)
 
 
+#
 # %load_ext tensorboard
 # %tensorboard --logdir {log_dir}
 
-fit(train_dataset, test_dataset, steps=500)
-for example_input, example_target in test_dataset.take(1):
-    generate_images(generator, example_input, example_target)
+fit(train_dataset, test_dataset, steps=10)
